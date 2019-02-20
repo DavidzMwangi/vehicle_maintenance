e0d91cc551387dc584d50cc2a5e30443c7e134ff
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-02-20 11:38:41 +0300

    session handling

--- src/java/dao/WorkOrderDao.java
+++ src/java/dao/WorkOrderDao.java
@@ -37,15 +37,9 @@ public class WorkOrderDao {
     
     
      public static List<WorkOrder> fetchWorkOrder(Connection connection){
-//        String sql="SELECT * FROM work_order";
-        String sql="SELECT work_order.id,work_order.created_at,work_order.work_instructions,work_order.servicing,vehicles.reg_no FROM work_order LEFT JOIN vehicles  ON work_order.vehicle_id=vehicles.id";
-//        
-        /*
-        SELECT A.`first_name` , A.`last_name` , B.`title`
-FROM `members`AS A
-INNER JOIN `movies` AS B
-ON B.`id` = A.`movie_id`
-        */
+
+         String sql="SELECT work_order.id,work_order.created_at,work_order.work_instructions,work_order.servicing,work_order.completed,vehicles.reg_no FROM work_order LEFT JOIN vehicles  ON work_order.vehicle_id=vehicles.id";
+
         
          List<WorkOrder> workorder = null;
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -69,6 +63,7 @@ ON B.`id` = A.`movie_id`
        workorder.setRegNo(resultSet.getString("reg_no"));
       workorder.setServicing(resultSet.getBoolean("servicing"));
        workorder.setWorkInstructions(resultSet.getString("work_instructions"));
+       workorder.setCompleted(resultSet.getBoolean("completed"));
 
             workorders.add(workorder);
         }
@@ -78,7 +73,7 @@ ON B.`id` = A.`movie_id`
      public static List<WorkOrder> mechanicWorkOrders(Connection connection,int mechanic_id){
          
 //         String sql="SELECT * FROM work_order WHERE mechanic_id =?";
-                 String sql="SELECT work_order.id,work_order.created_at,work_order.work_instructions,work_order.servicing,vehicles.reg_no FROM work_order  LEFT JOIN vehicles  ON work_order.vehicle_id=vehicles.id WHERE work_order.mechanic_id =?";
+                 String sql="SELECT work_order.id,work_order.created_at,work_order.work_instructions,work_order.servicing,work_order.completed,vehicles.reg_no FROM work_order  LEFT JOIN vehicles  ON work_order.vehicle_id=vehicles.id WHERE work_order.mechanic_id =?";
 
          List<WorkOrder> workOrders=null;
          try (PreparedStatement statement = connection.prepareStatement(sql)){
@@ -94,17 +89,17 @@ ON B.`id` = A.`movie_id`
          }
      }
      
-//     private static void setMechanicWorkOrder(List<WorkOrder> workorders, ResultSet resultSet) throws SQLException {
-//        while (resultSet.next()) {
-//
-//           
-//       WorkOrder workorder=new WorkOrder();
-//       workorder.setId(resultSet.getInt("id"));
-//       workorder.setCreatedAt(resultSet.getString("created_at"));
-//       workorder.setRegNo(resultSet.getString("reg_no"));
-//       workorder.setWorkInstructions(resultSet.getString("work_instructions"));
-//
-//            workorders.add(workorder);
-//        }
-//    }
+     public static void markCompleted(Connection connection,int workOrderId) throws SQLException {
+       
+         String sql="UPDATE work_order set completed=? where id=?";
+         try(PreparedStatement statement=connection.prepareStatement(sql)){
+             statement.setBoolean(1, true);
+             statement.setInt(2,workOrderId);
+             statement.executeUpdate();
+             
+         }catch(SQLException e){
+             e.printStackTrace();
+         }
+         
+    }
 }
